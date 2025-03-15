31d3bc17b42e8663741608ea74df28ad383d4c1a
santimontironi <santiimontironi@gmail.com>
2025-03-15 18:40:16 -0300

    se creo el formulario del modelo Perfil

--- redsocialapp/forms.py
+++ redsocialapp/forms.py
@@ -1,7 +1,11 @@
 from .models import Perfil
 from django import forms
 
-class PerfilForm(forms.Form):
+class PerfilForm(forms.ModelForm):
     class Meta:
         model = Perfil
-        fields = ['nombre','imagen']
+        fields = ['imagen', 'nombre', 'descripcion']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Tu nombre'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Agrega una descripción'}),
+        }
--- redsocialapp/views.py
+++ redsocialapp/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.decorators import login_required
+from .forms import PerfilForm
 
 # Create your views here.
 def ingreso(request):
@@ -32,7 +33,7 @@ def registro(request):
                 usuarioRegistrado = User.objects.create_user(username = username, password = clave1)
                 usuarioRegistrado.save()
                 login(request,usuarioRegistrado)
-                return redirect('ingreso')
+                return redirect('crear-perfil')
             except IntegrityError:
                 return render(request,'registro.html',{
                     'errorUsuarioExistente':"Usuario ya existente con el username ingresado. Por favor vuelva a ingrese otro nuevamente."
@@ -44,8 +45,18 @@ def registro(request):
             
 @login_required
 def crearPerfil(request):
-    if request.method == "GET":
-        return render(request,'crearPerfil.html')
+    if request.method == "POST":
+        form = PerfilForm(request.POST, request.FILES)  # Crear el formulario con los datos ingresados
+        perfil = form.save(commit=False)  # No guarda aún, permite modificar antes
+        perfil.user = request.user  # Asocia el perfil con el usuario actual
+        perfil.save()  # Guarda finalmente
+        return redirect('inicio')
+    else:
+        form = PerfilForm()  # Muestra un formulario vacío para crear el perfil
+        return render(request,'crearPerfil.html',{
+            'form':form
+        })
+
             
 @login_required            
 def inicio(request):
