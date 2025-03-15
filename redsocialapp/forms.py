b313b11d511d68bddae04ba949a184040c84d022
santimontironi <santiimontironi@gmail.com>
2025-03-15 18:48:08 -0300

    se elimino la lista widgets de PerfilForm

--- redsocialapp/forms.py
+++ redsocialapp/forms.py
@@ -5,7 +5,3 @@ class PerfilForm(forms.ModelForm):
     class Meta:
         model = Perfil
         fields = ['imagen', 'nombre', 'descripcion']
-        widgets = {
-            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Tu nombre'}),
-            'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Agrega una descripción'}),
-        }
