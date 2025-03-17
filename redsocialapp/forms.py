149c1fcea0547995a280f843449bdf7996a634c6
santimontironi <santiimontironi@gmail.com>
2025-03-17 19:53:44 -0300

    en forms se agrego el formulario de Publicacion

--- redsocialapp/forms.py
+++ redsocialapp/forms.py
@@ -1,7 +1,12 @@
-from .models import Perfil
+from .models import Perfil,Publicacion
 from django import forms
 
 class PerfilForm(forms.ModelForm):
     class Meta:
         model = Perfil
         fields = ['imagen', 'nombre', 'apellido', 'descripcion']
+        
+class PublicacionForm(forms.ModelForm):
+    class Meta:
+        model = Publicacion
+        fields = ['imagen', 'descripcion']
