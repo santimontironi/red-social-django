6bda36658abd2361bce51d05d4e29aaa5c1a5a16
santimontironi <santiimontironi@gmail.com>
2025-04-01 11:38:06 -0300

    se agrego la comprobacion de no internet en contenedores

--- redsocialapp/static/js/script.js
+++ redsocialapp/static/js/script.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const contenedorSinInternet = document.querySelector(".contenedorSinInternet");
+    const contenedorRegistro = document.getElementById("contenedorRegistro")
+    const contenedorLogin = document.getElementById("contenedorLogin");
+
 if(!navigator.onLine){
-    alert("Estás sin conexión a internet.")
+    if (contenedorLogin) contenedorLogin.style.display = "none"
+    if (contenedorRegistro) contenedorRegistro.style.display = "none"
+    if (contenedorSinInternet){
+        contenedorSinInternet.classList.remove("d-none")
+        contenedorSinInternet.classList.add("d-flex")
+    }
 }
+})
+
