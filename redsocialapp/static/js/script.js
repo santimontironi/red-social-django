74ba45c899047ec3b9cad25436efb86763f42200
santimontironi <santiimontironi@gmail.com>
2025-04-03 23:55:40 -0300

    manej bien del nav al apretar en el buscador

--- redsocialapp/static/js/script.js
+++ redsocialapp/static/js/script.js
@@ -24,50 +24,70 @@ document.addEventListener("DOMContentLoaded",function(){
         }
     }
     //Los if para cada contenedor sirven para asegurarse de que los elementos existen en el DOM antes de intentar modificar sus estilos.
-})
 
-//Bloque de codigo para mostrar el contenedor de agregar comentarios en una publicacion.
+    //Bloque de codigo para mostrar el contenedor de agregar comentarios en una publicacion.
 
-const btnComentario = document.getElementById("btnComentario")
-const contenedorComentario = document.getElementById("contenedorComentario")
-const btnCerrarComentario = document.querySelector(".btnCerrarComentario")
+    const btnComentario = document.getElementById("btnComentario")
+    const contenedorComentario = document.getElementById("contenedorComentario")
+    const btnCerrarComentario = document.querySelector(".btnCerrarComentario")
 
-btnComentario.addEventListener("click",function(){
-    contenedorComentario.style.display = "flex"
-})
+    btnComentario.addEventListener("click",function(){
+        contenedorComentario.style.display = "flex"
+    })
 
-btnCerrarComentario.addEventListener("click",function(){
-    contenedorComentario.style.display = "none"
-})
+    btnCerrarComentario.addEventListener("click",function(){
+        contenedorComentario.style.display = "none"
+    })
 
-//Bloque de codigo para mostrar el formulario de busqueda de usuarios.
 
-const btnBusquedaUsuarios = document.querySelector(".btnBusquedaUsuarios")
-const formBusqueda = document.getElementById("formBusqueda")
-const header = document.getElementById("header")
+    const btnBusquedaUsuarios = document.querySelector(".btnBusquedaUsuarios")
+    const btnCerrarFormBusqueda = document.querySelector(".btnCerrarFormBusqueda")
+    const formBusqueda = document.getElementById("formBusqueda")
+    const header = document.getElementById("header")
 
-header.addEventListener("mouseover",function(){
-    if (formBusqueda.classList.contains("formBusquedaAbierto")){
-        header.style.width = "380px"
-    } else{
-        header.style.width = "320px"
+    //variable de control para saber si el formulario de busqueda esta activo o no.
+    let busquedaActiva = false
+
+    //El evento mouseover se activa cuando el mouse entra en el area del elemento, y el evento mouseout se activa cuando el mouse sale del area del elemento.
+    //Esto permite que el header se expanda cuando el mouse entra en el area del elemento, y se contraiga cuando el mouse sale del area del elemento, pero solo si el formulario de busqueda no esta activo.
+    if (header){
+        header.addEventListener("mouseover",function(){
+            if (!busquedaActiva){
+                header.style.width = "300px"
+            }
+        })
+        header.addEventListener("mouseout",function(){
+            if (!busquedaActiva){
+                header.style.width = "147px"
+            }
+        })
     }
-})
-
-header.addEventListener("mouseleave",function(){
-    header.style.width = "147px"
-})
-
-btnBusquedaUsuarios.addEventListener("click",function(){
-    formBusqueda.classList.toggle("formBusquedaAbierto")
-
-    if (formBusqueda.classList.contains("formBusquedaAbierto")){
-        formBusqueda.style.display = "none"
-    }else{
-        formBusqueda.style.display = "flex"
-        formBusqueda.style.flexDirection = "column"
-        formBusqueda.style.alignItems = "center"
-        formBusqueda.style.justifyContent = "center"
-        formBusqueda.style.gap = "6px"
+    
+    //El evento click se activa cuando el usuario hace click en el elemento.
+    //Esto permite que el header se expanda cuando el usuario hace click en el elemento.
+    if(btnBusquedaUsuarios){
+        btnBusquedaUsuarios.addEventListener("click",function(){
+            formBusqueda.style.display = "flex"
+            formBusqueda.style.flexDirection = "column"
+            formBusqueda.style.alignItems = "center"
+            formBusqueda.style.textAlign = "center"
+            formBusqueda.style.justifyContent = "center"
+            formBusqueda.style.gap = "10px"
+            header.style.width = "410px"
+            busquedaActiva = true
+        })
     }
+
+    //El evento click se activa cuando el usuario hace click en el elemento.
+    //Esto permite que el header se contraiga cuando el usuario hace click en el elemento.
+    if(btnCerrarFormBusqueda){
+        btnCerrarFormBusqueda.addEventListener("click",function(){
+            formBusqueda.style.display = "none"
+            header.style.width = "147px"
+            busquedaActiva = false
+        })
+    }
+    
+
 })
+
