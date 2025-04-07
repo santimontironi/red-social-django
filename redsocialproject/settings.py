dd9ef33374023f44c66f8f255d2e67ae20567091
santimontironi <santiimontironi@gmail.com>
2025-04-06 22:42:21 -0300

    se agrego un archivo .env para mejorar las variables para configuraciones personales

--- redsocialproject/settings.py
+++ redsocialproject/settings.py
@@ -145,10 +145,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Configuración de correo electrónico
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True 
-EMAIL_HOST_USER = "santiimontironi@gmail.com"
-EMAIL_HOST_PASSWORD = "gwgf bnld vrfw gzpm"
+EMAIL_BACKEND = config("EMAIL_BACKEND")
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
